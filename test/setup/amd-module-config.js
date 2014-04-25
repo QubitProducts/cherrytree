09d1668382d66e40489ce8c34dad6539524836bd
Karolis Narkevicius <karolis.n@gmail.com>
2014-04-25 13:54:01 +0100

    Back to rsvp - when/lib/Promise error behaviour different after all

--- test/setup/amd-module-config.js
+++ test/setup/amd-module-config.js
@@ -2,8 +2,7 @@ require.config({
   paths: {
     "underscore": "bower_components/underscore/underscore",
     "jquery": "bower_components/jquery/jquery",
-    "location-bar": "bower_components/location-bar/location-bar",
-    "when": "bower_components/when"
+    "location-bar": "bower_components/location-bar/location-bar"
   },
   packages: [{
     name: "cherrytree", location: ".", main: "router"
