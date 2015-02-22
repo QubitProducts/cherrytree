0674e01d63aaa0d18a31388109cf170bc1946cab
Karolis Narkevicius <karolis.n@gmail.com>
2015-02-22 14:17:02 +0000

    Fix the redirect in posts.index handler

--- examples/vanilla-blog/client/screens/app/screens/posts/screens/index/index.js
+++ examples/vanilla-blog/client/screens/app/screens/posts/screens/index/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  model: function (params, transition) {
+  model: function (params, context, transition) {
     transition.redirectTo("posts.show", 1);
   },
   activate: function () {},
