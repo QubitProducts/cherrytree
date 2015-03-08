ed2805547a29daf273e448a01a50aaac08edf22e
Karolis Narkevicius <karolis.n@gmail.com>
2015-03-08 19:04:52 +0000

    Fix the regression when queryParams are empty

--- tests/routerTest.js
+++ tests/routerTest.js
@@ -78,13 +78,46 @@ test('#generate throws a useful error when listen has not been called', () => {
 test('#match matches a path against the routes', () => {
   assert.expect(2)
   router.map(routes)
-  let match = router.match('/application/KidkArolis/status/42?withReplies=false')
+  let match = router.match('/application/KidkArolis/status/42')
+  assert.equals(match.params, {
+    user: 'KidkArolis',
+    id: '42',
+    queryParams: {}
+  })
+  assert.equals(_.pluck(match.routes, 'name'), ['application', 'status'])
+})
+
+test('#match matches a path with query params', () => {
+  assert.expect(1)
+  router.map(routes)
+  let match = router.match('/application/KidkArolis/status/42?withReplies=true&foo=bar')
   assert.equals(match.params, {
     user: 'KidkArolis',
     id: '42',
     queryParams: {
-      withReplies: 'false'
+      withReplies: 'true',
+      foo: 'bar'
     }
   })
-  assert.equals(_.pluck(match.routes, 'name'), ['application', 'status'])
 })
+
+suite('route maps')
+
+test('routes with name "index" or that end int ".index" default to an empty path', () => {
+  router.map(function () {
+    this.route('index')
+    this.route('foo')
+    this.route('bar', function () {
+      this.route('bar.index')
+    })
+  })
+  assert.equals(_.pluck(router.matchers, 'path'), [
+    '/',
+    '/foo',
+    '/bar'
+  ])
+})
+
+test('routes with duplicate names throw a useful error')
+
+test('the path of the first top level route defaults to /')
